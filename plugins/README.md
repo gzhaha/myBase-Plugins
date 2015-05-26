cf7b634bbd5124d13d2d1ebdd6c4804117ff42ef
gzhaha <gzhaha@gmail.com>
2015-05-26 12:47:30 +1000

    Upload Plugin SetFontSize

--- plugins/README.md
+++ plugins/README.md
@@ -1,4 +1,4 @@
-# myBase Plugins Folder
+# Plugins
 
 This folder contains all the plugins (**NOT OFFICIAL PLUGINS**) for myBase 7.x
  
@@ -7,4 +7,11 @@ This folder contains all the plugins (**NOT OFFICIAL PLUGINS**) for myBase 7.x
 2. Copy the .js file to \plugins folder.  
 3. Run the software again.  
 4. Based on the setting of the plugins, you can find the new functions from different menu.
+
+## **Plugins Lists** ##
+### 1. SetFontSize ###  
+This plugin Can be used to set the font size of current info item.  
+Please note, this plugin will change all the font size of the current info item and **do not have a reverse function**, meaning you **cannot use** `crtl+z` to reverse back. Currently, the font size can be set between 5-40pt.  
+You can find this function (**Set Font Size**) by right click in the input area. 
+
 
---
+++ plugins/SetFontSize.js
@@ -0,0 +1,45 @@
+﻿
+//sValidation=nyfjs
+//sCaption=Set Font Size
+//sHint=Set Font Size 26052015
+//sCategory=Context.HtmlEdit
+//sPosition=TB-255
+//sCondition=CURDB; DBRW; CURINFOITEM; HTMLEDIT
+//sID=p.Ins.GzhahaSetFontSize
+//sAppVerMin=7.0
+//sShortcutKey=
+//sAuthor=Xia Zhang
+
+var _lc=function(sTag, sDef){return plugin.getLocaleMsg(sTag, sDef);};
+var _lc2=function(sTag, sDef){return _lc(plugin.getLocaleID()+'.'+sTag, sDef);};
+
+var _trim=function(s){return (s||'').replace(/^\s+|\s+$/g, '');};
+var _trim_cr=function(s){return (s||'').replace(/\r+$/g, '');};
+
+try{
+	var xNyf=new CNyfDb(-1);
+	if(xNyf.isOpen()){
+		if(!xNyf.isReadonly()){
+			if(plugin.isContentEditable()){
+				//get selected text
+				var sCon = plugin.getTextContent(-1, true);
+				var sSiz = prompt('Input Font Size (5-40)：', '16', 'Input Font Size');
+				if (sSiz>=5 && sSiz<=40){
+					var html = sCon.replace(/font-size:(.*?)pt/g, 'font-size: '+ sSiz + 'pt')
+					plugin.setTextContent(-1, html, true);
+				}
+				else{
+					alert("Value should be 5-40!")
+				}
+			}else{
+				alert(_lc('Prompt.Warn.ReadonlyContent', 'Cannot modify the content opened as Readonly.'));
+			}
+		}else{
+			alert(_lc('Prompt.Warn.ReadonlyDb', 'Cannot modify the database opened as Readonly.'));
+		}
+	}else{
+		alert(_lc('Prompt.Warn.NoDbOpened', 'No database is currently opened.'));
+	}
+}catch(e){
+	alert(e);
+}
