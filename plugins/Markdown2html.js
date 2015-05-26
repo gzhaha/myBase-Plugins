646d1efe7a4bdf8eed718ea9c9ffca8859cb6ed9
gzhaha <gzhaha@gmail.com>
2015-05-26 18:57:01 +1000

    Markdown plugins for myBase

---
+++ plugins/Markdown2html.js
@@ -0,0 +1,54 @@
+﻿
+//sValidation=nyfjs
+//sCaption=md2html
+//sHint=MarkDown2html 26052015
+//sCategory=MainMenu.TxtUtils
+//sPosition=TB-255
+//sCondition=CURDB; DBRW; CURINFOITEM; HTMLEDIT; HTMLSELECTED
+//sID=p.gzhaha.MarkDown2html
+//sAppVerMin=7.0
+//sShortcutKey=
+//sAuthor=Xia Zhang
+
+//This script can change the selected markdown style text to html by utilizing marked.js 
+
+var _lc=function(sTag, sDef){return plugin.getLocaleMsg(sTag, sDef);};
+var _lc2=function(sTag, sDef){return _lc(plugin.getLocaleID()+'.'+sTag, sDef);};
+
+var _trim=function(s){return (s||'').replace(/^\s+|\s+$/g, '');};
+var _trim_cr=function(s){return (s||'').replace(/\r+$/g, '');};
+
+try{
+	var xNyf=new CNyfDb(-1);
+	if(xNyf.isOpen()){
+		if(!xNyf.isReadonly()){
+			if(plugin.isContentEditable()){
+				//get selected text
+				var sCon = plugin.getSelectedText(-1, false);
+				
+				//formats(table,code,blockquote)
+				var sCode='\n'+'var xStyle=document.getElementsByTagName("style")[0]; if(xStyle.innerHTML.indexOf("code") < 0) {var t = document.createTextNode("\\ntable{border-collapse:collapse; border:solid black; border-width:2px 0 2px 1px;}\\nth, td {border:solid black;border-width:2px 1px 1px 0;padding:1px;}\\nth {background-color:#DDD}\\ntable tr:nth-child(2n) {background-color: #f8f8f8;}\\nblockquote{border-left: 6px solid #DDD; color:#777}\\ncode {font-size: 98%;background-color: rgba(0, 0, 0, 0.04);border-radius: 3px;}\\np{font-weight:normal}"); xStyle.appendChild(t); document.head.appendChild(xStyle);}; ';
+				plugin.runDomScript(-1, sCode);
+				
+				//load marked.js
+				var sFile = plugin.getScriptFile();
+				var jsPath = sFile.substring(0,sFile.lastIndexOf('/'));
+				var sTxt=platform.parseFile(jsPath+"/marked.js");
+				eval.call(null, sTxt);
+				
+				//parse markdown text to html
+				var html = marked(sCon);
+				plugin.replaceSelectedText(-1, html, true);		
+
+			}else{
+				alert(_lc('Prompt.Warn.ReadonlyContent', 'Cannot modify the content opened as Readonly.'));
+			}
+		}else{
+			alert(_lc('Prompt.Warn.ReadonlyDb', 'Cannot modify the database opened as Readonly.'));
+		}
+	}else{
+		alert(_lc('Prompt.Warn.NoDbOpened', 'No database is currently opened.'));
+	}
+}catch(e){
+	alert(e);
+}
---
+++ plugins/Markdownattach.js
@@ -0,0 +1,54 @@
+﻿
+//sValidation=nyfjs
+//sCaption=.mdatt2InfoItem
+//sHint=load Markdown Attachment to InfoItem 26052015
+//sCategory=MainMenu.Edit;Context.Attachments
+//sPosition=TB-255
+//sCondition=CURDB
+//sID=p.gzhaha.MarkDownAtta
+//sAppVerMin=7.0
+//sShortcutKey=
+//sAuthor=Xia Zhang
+
+try{
+	if(plugin.getCurDbIndex()>=0){
+		
+		var nyf=new CNyfDb(-1);
+		var f=new CLocalFile(plugin.getCurInfoItem(), 'note.md');
+		var sRes=plugin.getSelectedAttachments('\t')||'';
+		if(sRes){
+			var vLines=sRes.split('\n');
+			for(var i in vLines){
+				var v=(vLines[i]||'').split('\t');
+				var sDbPath=v[0], sSsgPath=v[1], sSsgName=v[2];
+				var xFn=new CLocalFile(sSsgPath); xFn.append(sSsgName);
+					sFnSel=xFn.toString();
+			}
+		}else{
+			var xFn=new CLocalFile(plugin.getCurDocFile());
+			sFnSel=xFn.toString();			
+		}
+		if(sFnSel){
+			var sRtf=nyf.loadText(sFnSel,'utf-8');
+		}
+		var sFile = plugin.getScriptFile(); 
+		var jsPath = sFile.substring(0,sFile.lastIndexOf('/'));
+		var sTxt=platform.parseFile(jsPath+"/marked.js");
+		eval.call(null, sTxt);
+		
+		var html = marked(sRtf);
+		plugin.setTextContent(-1, html, true);
+		
+		//formats(table,code,blockquote)
+		var sCode='\n'+'var x = document.createElement("STYLE"); var t = document.createTextNode("body, table{font-family: 微软雅黑; font-size: 18pt}\\ntable {border-collapse:collapse;border:solid black;border-width:2px 0 2px 1px;}\\nth, td {border:solid black;border-width:2px 1px 1px 0;padding:1px;}\\nth {background-color:#DDD}\\ntable tr:nth-child(2n) {background-color: #f8f8f8;}\\nblockquote{border-left: 6px solid #DDD; color:#777}\\ncode {font-size: 98%;background-color: rgba(0, 0, 0, 0.04);border-radius: 3px;}\\np{font-weight:normal}"); x.appendChild(t); document.head.appendChild(x);';
+		plugin.runDomScript(-1, sCode);
+		
+		plugin.setDomDirty(-1, true);
+				
+	}else{
+		alert(_lc('Prompt.Warn.NoDbOpened', 'No database is currently opened.'));
+	}
+}catch(e){
+	alert(e);
+}
+
