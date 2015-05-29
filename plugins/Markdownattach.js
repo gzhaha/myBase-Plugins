56ca6654635950b0697b6b21ef2c31788a9e5af9
gzhaha <gzhaha@gmail.com>
2015-05-30 08:32:18 +1000

    minor updates

--- plugins/Markdownattach.js
+++ plugins/Markdownattach.js
@@ -1,7 +1,7 @@
 ﻿
 //sValidation=nyfjs
 //sCaption=.mdatt2InfoItem
-//sHint=load Markdown Attachment to InfoItem 26052015
+//sHint=Load .MD Attachment to InfoItem 29052015
 //sCategory=MainMenu.Edit;Context.Attachments
 //sPosition=TB-255
 //sCondition=CURDB
@@ -31,10 +31,11 @@ try{
 		if(sFnSel){
 			var sRtf=nyf.loadText(sFnSel,'utf-8');
 		}
-		var sFile = plugin.getScriptFile(); 
+		var sFile = plugin.getScriptFile();
 		var jsPath = sFile.substring(0,sFile.lastIndexOf('/'));
-		var sTxt=platform.parseFile(jsPath+"/marked.js");
-		eval.call(null, sTxt);
+		var s=new CLocalFile(jsPath+"/marked.js");
+		var sJs=s.loadText();
+		eval.call(null, sJs);
 		
 		var html = marked(sRtf);
 		plugin.setTextContent(-1, html, true);
@@ -42,7 +43,6 @@ try{
 		//formats(table,code,blockquote)
 		var sCode='\n'+'var x = document.createElement("STYLE"); var t = document.createTextNode("body, table{font-family: 微软雅黑; font-size: 18pt}\\ntable {border-collapse:collapse;border:solid black;border-width:2px 0 2px 1px;}\\nth, td {border:solid black;border-width:2px 1px 1px 0;padding:1px;}\\nth {background-color:#DDD}\\ntable tr:nth-child(2n) {background-color: #f8f8f8;}\\nblockquote{border-left: 6px solid #DDD; color:#777}\\ncode {font-size: 98%;background-color: rgba(0, 0, 0, 0.04);border-radius: 3px;}\\np{font-weight:normal}"); x.appendChild(t); document.head.appendChild(x);';
 		plugin.runDomScript(-1, sCode);
-		
 		plugin.setDomDirty(-1, true);
 				
 	}else{
