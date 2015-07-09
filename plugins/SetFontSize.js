f745ecca974b3868c131e94123a6ce547bcb0e3b
gzhaha <gzhaha@gmail.com>
2015-07-09 21:57:31 +1000

    Minor Code Optimization

--- plugins/SetFontSize.js
+++ plugins/SetFontSize.js
@@ -1,7 +1,7 @@
 
 //sValidation=nyfjs
 //sCaption=Set Font Size
-//sHint=Set Font Size 11062015
+//sHint=Set Font Size 09072015
 //sCategory=Context.HtmlEdit
 //sPosition=XZ-255
 //sCondition=CURDB; DBRW; CURINFOITEM; HTMLEDIT
@@ -29,20 +29,22 @@ try{
 				//get text
 				var sCon = plugin.getTextContent(-1, true);
 				var sSiz = prompt('Input Font Size (5-40):', '16', 'Input Font Size');
-				if (sSiz>=5 && sSiz<=40){
+				if (sSiz){
+					if (sSiz>=5 && sSiz<=40){
 
-					//match px, pt, %
-					var regx = /font-size:( |)\d{1,2}(|\.\d+)(| )pt|font-size:( |)\d{1,2}(|\.\d+)(| )px|font-size:( |)\d{1,3}(| )%/g
-					var html = sCon.replace(regx, 'font-size: '+ sSiz + 'pt')
+						//match px, pt, %
+						var regx = /font-size:( |)\d{1,2}(|\.\d+)(| )pt|font-size:( |)\d{1,2}(|\.\d+)(| )px|font-size:( |)\d{1,3}(| )%/g;
+						var html = sCon.replace(regx, 'font-size: '+ sSiz + 'pt');
 
-					//2015.6.12 'setHTML' clears DOM entirely including UNDO stack, so it'd be worth to first save current modifications as a history revision for UNDOable;
-					plugin.commitCurrentChanges();
+						//2015.6.12 'setHTML' clears DOM entirely including UNDO stack, so it'd be worth to first save current modifications as a history revision for UNDOable;
+						plugin.commitCurrentChanges();
 
-					plugin.setTextContent(-1, html, true);
-					plugin.setDomDirty(-1, true);
-				}
-				else{
-					alert("Value should be 5-40!")
+						plugin.setTextContent(-1, html, true);
+						plugin.setDomDirty(-1, true);
+					}
+					else{
+						alert("Value should be 5-40!");
+					}
 				}
 			}else{
 				alert(_lc('Prompt.Warn.ReadonlyContent', 'Cannot modify the content opened as Readonly.'));
